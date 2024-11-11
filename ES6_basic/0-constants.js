652585359182e3a78d920c72a35aba721eb40d70
Gromuf <luigi.beaumois@gmail.com>
2024-11-11 14:23:27 +0100

    aaaa

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,18 @@
-// 0-constant.js
+// 0-constants.js
 
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
+	const task = 'I prefer const when I can.';  // Using const since the variable does not change
 	return task;
-}
-
-export function getLast() {
+  }
+  
+  export function getLast() {
 	return ' is okay';
-}
-
-export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
-
+  }
+  
+  export function taskNext() {
+	let combination = 'But sometimes let';  // Using let because we modify the variable
+	combination += getLast();  // Concatenating a string to combination
+  
 	return combination;
-}
+  }
+  
