60c08e520255aa483df9f9dbeaec63f0a06c6a75
Gromuf <luigi.beaumois@gmail.com>
2025-09-25 10:48:20 +0200

    task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+function createClassroom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    };
+  }
+  let students = [];
+  for (let i = 0; i < numbersOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  }
+  return students;
+}
+let classRoom = createClassroom(10);
